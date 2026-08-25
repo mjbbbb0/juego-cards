aded2e60ba58c8522639a347056685ab28e529df
mjbbbb0 <mejorbot@gmail.com>
2026-08-26 00:49:05 +0200

    Actualizar main.js

--- js/main.js
+++ js/main.js
@@ -1,34 +1,10 @@
-import {
-    resetGameState
-} from "./gameState.js";
+import { showScreen } from "./screens/screenManager.js";
 
-import {
-    showScreen
-} from "./screens/screenManager.js";
+import { createMenuScreen } from "./screens/menuScreen.js";
 
 
-const newGameButton =
-    document.getElementById("new-game");
-
-const continueButton =
-    document.getElementById("continue-game");
+const menu =
+    createMenuScreen();
 
 
-// Nueva partida
-
-newGameButton.addEventListener("click", () => {
-
-    resetGameState();
-
-    console.log("Nueva partida iniciada");
-
-});
-
-
-// Continuar partida
-
-continueButton.addEventListener("click", () => {
-
-    console.log("Continuar partida");
-
-});
+showScreen(menu);
