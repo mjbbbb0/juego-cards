2fcb76acb97da7d4401563ecd9923b783b7715fe
mjbbbb0 <mejorbot@gmail.com>
2026-08-26 01:59:40 +0200

    Actualizar mapConfig.js

--- js/map/mapConfig.js
+++ js/map/mapConfig.js
@@ -1,17 +1,59 @@
 export const mapConfig = {
 
-    floors: 15,
+    rows: 15,
 
-    minNodesPerFloor: 2,
+    columns: 7,
 
-    maxNodesPerFloor: 4,
 
-    nodeTypes: [
-        "combat",
-        "event",
-        "rest",
+    // Habitaciones iniciales
+    startingRooms: 6,
+
+
+    // Número de destinos posibles
+    maxNextRooms: 3,
+
+
+    // Probabilidades
+    locationOdds: {
+
+        combat: 45,
+        event: 22,
+        elite: 16,
+        rest: 12,
+        shop: 5,
+        chest: 0
+
+    },
+
+
+    // Pisos especiales
+
+    treasureFloor: 9,
+
+    restFloor: 15,
+
+
+    // No puede haber estos tipos
+    // antes del piso indicado
+
+    minimumEliteFloor: 6,
+
+    minimumRestFloor: 6,
+
+
+    // Descanso no permitido aquí
+
+    restForbiddenFloor: 14,
+
+
+    // Tipos que no pueden ser consecutivos
+
+    nonConsecutiveTypes: [
+
         "elite",
-        "shop"
+        "shop",
+        "rest"
+
     ]
 
 };
