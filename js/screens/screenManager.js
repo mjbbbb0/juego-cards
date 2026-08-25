dd1a5e91d7bb4d0e7eeaf96f1467c1c9e65279f3
mjbbbb0 <mejorbot@gmail.com>
2026-08-26 00:48:36 +0200

    Actualizar screenManager.js

--- js/screens/screenManager.js
+++ js/screens/screenManager.js
@@ -1,46 +1,26 @@
-const screenContainer =
-    document.getElementById("screen-container");
-
-const menu =
-    document.getElementById("menu");
-
 let currentScreen = null;
 
 
-// ========================================
-// MOSTRAR PANTALLA
-// ========================================
+const game =
+    document.getElementById("game");
+
 
 export function showScreen(screen) {
 
-    // Eliminar pantalla actual
+    // Eliminar pantalla anterior
+
     if (currentScreen) {
+
         currentScreen.remove();
-        currentScreen = null;
+
     }
 
-    // Ocultar menú
-    menu.classList.add("hidden");
 
-    // Añadir nueva pantalla
-    screenContainer.appendChild(screen);
+    // Mostrar nueva pantalla
+
+    game.appendChild(screen);
+
 
     currentScreen = screen;
-}
 
-
-// ========================================
-// MOSTRAR MENÚ
-// ========================================
-
-export function showMenu() {
-
-    // Eliminar pantalla actual
-    if (currentScreen) {
-        currentScreen.remove();
-        currentScreen = null;
-    }
-
-    // Mostrar menú
-    menu.classList.remove("hidden");
 }
