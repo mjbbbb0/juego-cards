dabcd130e7b22f3b83f69f15e49ae58e18604619
mjbbbb0 <mejorbot@gmail.com>
2026-08-26 00:36:28 +0200

    Actualizar main.js

--- js/main.js
+++ js/main.js
@@ -1,73 +1,33 @@
-const newGameButton = document.getElementById("new-game");
-const continueGameButton = document.getElementById("continue-game");
-
-// Comprobar si existe una partida guardada
-function hasSaveGame() {
-    return localStorage.getItem("gameSave") !== null;
-}
-
-// Actualizar estado del botón Continuar
-function updateContinueButton() {
-    continueGameButton.disabled = !hasSaveGame();
-}
-
-updateContinueButton();
+import {
+    showScreen,
+    showMenu
+} from "./screens/screenManager.js";
 
 
+const newGameButton =
+    document.getElementById("new-game");
+
+const continueButton =
+    document.getElementById("continue-game");
+
+
+// ========================================
 // NUEVA PARTIDA
+// ========================================
+
 newGameButton.addEventListener("click", () => {
 
-    const saveData = {
-        player: {
-            name: "Héroe",
-            hp: 70,
-            maxHp: 70,
-            gold: 99
-        },
+    console.log("Nueva partida");
 
-        progress: {
-            floor: 1,
-            act: 1
-        },
-
-        deck: [
-            "strike",
-            "strike",
-            "defend",
-            "defend",
-            "bash"
-        ]
-    };
-
-    localStorage.setItem(
-        "gameSave",
-        JSON.stringify(saveData)
-    );
-
-    console.log("Nueva partida creada");
-
-    // De momento mostramos esto.
-    // Más adelante cambiaremos a la pantalla del mapa.
-    alert("Nueva partida");
 });
 
 
+// ========================================
 // CONTINUAR PARTIDA
-continueGameButton.addEventListener("click", () => {
+// ========================================
 
-    const save = localStorage.getItem("gameSave");
+continueButton.addEventListener("click", () => {
 
-    if (!save) {
-        return;
-    }
+    console.log("Continuar partida");
 
-    const gameData = JSON.parse(save);
-
-    console.log("Partida cargada:", gameData);
-
-    alert(
-        `Continuando partida...\n` +
-        `Piso: ${gameData.progress.floor}\n` +
-        `Vida: ${gameData.player.hp}/${gameData.player.maxHp}`
-    );
 });
