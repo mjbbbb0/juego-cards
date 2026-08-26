85978b58a42b6651249f3d80d8011fe196b0b67b
mjbbbb0 <mejorbot@gmail.com>
2026-08-26 03:48:50 +0200

    Actualizar main.js

--- js/main.js
+++ js/main.js
@@ -2,5 +2,33 @@ const app = document.getElementById("app");
 
 app.innerHTML = `
     <h1>Mi Roguelike</h1>
-    <p>main.js funciona.</p>
+
+    <p>El juego funciona correctamente.</p>
+
+    <button id="newGameButton">
+        Nueva partida
+    </button>
+
+    <button id="continueButton">
+        Continuar partida
+    </button>
 `;
+
+const newGameButton =
+    document.getElementById("newGameButton");
+
+newGameButton.addEventListener("click", () => {
+
+    alert("Nueva partida");
+
+});
+
+
+const continueButton =
+    document.getElementById("continueButton");
+
+continueButton.addEventListener("click", () => {
+
+    alert("Continuar partida");
+
+});
