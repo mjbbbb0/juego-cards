fbdbe8674a4a894dd8d49089ddc0352d4dc8421f
mjbbbb0 <mejorbot@gmail.com>
2026-08-26 03:53:02 +0200

    Actualizar screenManager.js

--- js/screens/screenManager.js
+++ js/screens/screenManager.js
@@ -11,11 +11,35 @@ export class ScreenManager {
 
     show(screen) {
 
+        // Si hay una pantalla anterior,
+        // le damos la oportunidad de limpiarse.
+
+        if (this.currentScreen) {
+
+            if (this.currentScreen.destroy) {
+
+                this.currentScreen.destroy();
+
+            }
+
+        }
+
+
+        // Guardamos la nueva pantalla
+
         this.currentScreen = screen;
+
+
+        // Limpiamos el contenedor
 
         this.container.innerHTML = "";
 
-        screen.render(this.container);
+
+        // Dibujamos la nueva pantalla
+
+        this.currentScreen.render(
+            this.container
+        );
 
     }
 
