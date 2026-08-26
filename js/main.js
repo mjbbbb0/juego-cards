93caf35f817776f9976954f01a686e9bd30f17b3
mjbbbb0 <mejorbot@gmail.com>
2026-08-26 03:16:55 +0200

    Actualizar main.js

--- js/main.js
+++ js/main.js
@@ -2,20 +2,24 @@ import { ScreenManager } from "./screens/ScreenManager.js";
 import { MenuScreen } from "./screens/menuScreen.js";
 
 
+// Obtener el contenedor principal
+
 const app =
     document.getElementById("app");
 
+
+// Crear el ScreenManager
 
 const screenManager =
     new ScreenManager(app);
 
 
+// Crear el menú
+
 const menuScreen =
-    new MenuScreen(
-        screenManager
-    );
+    new MenuScreen(screenManager);
 
 
-screenManager.show(
-    menuScreen
-);
+// Mostrar el menú
+
+screenManager.show(menuScreen);
