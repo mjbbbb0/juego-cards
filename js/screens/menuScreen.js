0c87b6d578e95a5e29ab051e3b240e30e3cdc37e
mjbbbb0 <mejorbot@gmail.com>
2026-08-26 03:54:24 +0200

    Actualizar menuScreen.js

--- js/screens/menuScreen.js
+++ js/screens/menuScreen.js
@@ -1,59 +1,20 @@
 export class MenuScreen {
 
-    constructor(screenManager) {
-
-        this.screenManager = screenManager;
-
-    }
-
-
     render(container) {
 
         container.innerHTML = `
 
-            <div class="menu-screen">
+            <h1>Mi Roguelike</h1>
 
-                <h1>Mi Roguelike</h1>
+            <button>
+                Nueva partida
+            </button>
 
-                <button id="newGameButton">
-                    Nueva partida
-                </button>
-
-                <button id="continueButton">
-                    Continuar partida
-                </button>
-
-            </div>
+            <button>
+                Continuar partida
+            </button>
 
         `;
-
-
-        const newGameButton =
-            document.getElementById("newGameButton");
-
-
-        newGameButton.addEventListener(
-            "click",
-            () => {
-
-                console.log("Nueva partida");
-
-            }
-        );
-
-
-        const continueButton =
-            document.getElementById("continueButton");
-
-
-        continueButton.addEventListener(
-            "click",
-            () => {
-
-                console.log("Continuar partida");
-
-            }
-        );
 
     }
 
