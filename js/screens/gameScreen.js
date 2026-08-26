7c29229f14dc0dcbf8b875dff3600c5b3fd7d6f5
mjbbbb0 <mejorbot@gmail.com>
2026-08-26 03:11:47 +0200

    Actualizar gameScreen.js

--- js/screens/gameScreen.js
+++ js/screens/gameScreen.js
@@ -1,1 +1,52 @@
+import { gameState } from "../gameState.js";
 
+
+export class GameScreen {
+
+    constructor(screenManager) {
+
+        this.screenManager =
+            screenManager;
+
+    }
+
+
+    render(container) {
+
+        const player =
+            gameState.player;
+
+
+        container.innerHTML = `
+
+            <div class="game-screen">
+
+                <h1>
+                    Bienvenido,
+                    ${player.name}
+                </h1>
+
+                <p>
+                    ❤️
+                    ${player.hp}
+                    /
+                    ${player.maxHp}
+                </p>
+
+                <p>
+                    💰
+                    ${gameState.gold}
+                </p>
+
+                <p>
+                    Esta será la pantalla
+                    principal del juego.
+                </p>
+
+            </div>
+
+        `;
+
+    }
+
+}
