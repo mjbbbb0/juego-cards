88bee9a5ecdf829d0dd2ddec0bd1b35cb1ce8b0f
mjbbbb0 <mejorbot@gmail.com>
2026-08-26 03:06:25 +0200

    Actualizar gameState.js

--- js/gameState.js
+++ js/gameState.js
@@ -1,61 +1,7 @@
-import { generateRunSeed } from "./rng/seedManager.js";
-
-
 export const gameState = {
-
-    runSeed: null,
-
-    act: 1,
-
-    biomes: {
-        act1: null,
-        act2: null,
-        act3: null
-    },
-
-    maps: {
-        act1: null,
-        act2: null,
-        act3: null
-    },
 
     player: null,
 
-    currentNode: null
+    gold: 100
 
 };
-
-
-// ========================================
-// NUEVA PARTIDA
-// ========================================
-
-export function resetGameState() {
-
-    gameState.runSeed = generateRunSeed();
-
-    gameState.act = 1;
-
-    gameState.biomes = {
-        act1: null,
-        act2: null,
-        act3: null
-    };
-
-    gameState.maps = {
-        act1: null,
-        act2: null,
-        act3: null
-    };
-
-    gameState.player = null;
-
-    gameState.currentNode = null;
-
-
-    console.log(
-        "Nueva partida. Seed:",
-        gameState.runSeed
-    );
-
-}
