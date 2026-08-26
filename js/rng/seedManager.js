675d5a8fae184646db4efea9baa6c34cc8c153ba
mjbbbb0 <mejorbot@gmail.com>
2026-08-26 02:12:57 +0200

    Actualizar seedManager.js

--- js/rng/seedManager.js
+++ js/rng/seedManager.js
@@ -1,1 +1,9 @@
+// Genera una seed nueva para una partida
+export function generateRunSeed() {
 
+    const array = new Uint32Array(1);
+
+    crypto.getRandomValues(array);
+
+    return array[0];
+}
