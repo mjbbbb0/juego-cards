ba4c3abfa6888ebe2db8292534c90c96865a9bb9
mjbbbb0 <mejorbot@gmail.com>
2026-08-26 17:05:55 +0200

    Actualizar mapView.js

--- js/screens/views/mapView.js
+++ js/screens/views/mapView.js
@@ -1,6 +1,3 @@
-import { gameState } from "../gameState.js";
-
-
 export class MapView {
 
     constructor(screenManager) {
@@ -13,75 +10,17 @@ export class MapView {
 
     render(container) {
 
-        const maps =
-            gameState.map || {};
-
-
-        const map1 =
-            maps[1] || null;
-
-        const map2 =
-            maps[2] || null;
-
-        const map3 =
-            maps[3] || null;
-
-
         container.innerHTML = `
 
-            <div class="map-view">
+            <div>
 
                 <h1>
-                    MAPAS GENERADOS
+                    MAP VIEW FUNCIONA
                 </h1>
 
-
-                <h2>
-                    ACTO 1
-                </h2>
-
-                <pre>
-${map1
-    ? JSON.stringify(
-        map1,
-        null,
-        2
-    )
-    : "Mapa del Acto 1 no generado."
-}
-                </pre>
-
-
-                <h2>
-                    ACTO 2
-                </h2>
-
-                <pre>
-${map2
-    ? JSON.stringify(
-        map2,
-        null,
-        2
-    )
-    : "Mapa del Acto 2 no generado."
-}
-                </pre>
-
-
-                <h2>
-                    ACTO 3
-                </h2>
-
-                <pre>
-${map3
-    ? JSON.stringify(
-        map3,
-        null,
-        2
-    )
-    : "Mapa del Acto 3 no generado."
-}
-                </pre>
+                <p>
+                    Esta es la pantalla del mapa.
+                </p>
 
             </div>
 
