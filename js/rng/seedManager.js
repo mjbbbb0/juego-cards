6e7131fde3df3568e673c0eaa8822a1b349ca6b9
mjbbbb0 <mejorbot@gmail.com>
2026-08-26 04:48:42 +0200

    Eliminar seedManager.js

--- js/rng/seedManager.js
+++
@@ -1,9 +0,0 @@
-// Genera una seed nueva para una partida
-export function generateRunSeed() {
-
-    const array = new Uint32Array(1);
-
-    crypto.getRandomValues(array);
-
-    return array[0];
-}
