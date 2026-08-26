3bd70202763e2c8e215d7ea9eb7f85cf6f886ff5
mjbbbb0 <mejorbot@gmail.com>
2026-08-26 06:00:37 +0200

    Actualizar gridGenerator.js

--- js/map/generators/gridGenerator.js
+++ js/map/generators/gridGenerator.js
@@ -1,1 +1,45 @@
+import { MapNode } from "../mapNode.js";
+import { mapConfig } from "../mapConfig.js";
 
+
+export function generateGrid(map) {
+
+    let nodeId = 0;
+
+
+    // ========================================================
+    // CREAR GRID
+    // ========================================================
+
+    for (
+        let row = 1;
+        row <= mapConfig.rows;
+        row++
+    ) {
+
+        for (
+            let column = 0;
+            column < mapConfig.columns;
+            column++
+        ) {
+
+            const node =
+                new MapNode(
+                    `node_${nodeId}`,
+                    row,
+                    column
+                );
+
+
+            map.nodes.push(
+                node
+            );
+
+
+            nodeId++;
+
+        }
+
+    }
+
+}
